71645d4fc664b82a894351c685db82da229bea6c
mareq <mareq@balint.eu>
2017-05-16 16:59:56 +0100

    Context: Arrow functions

---
+++ 04-context/12-arrow-functions.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+
+// http://www.digital-web.com/articles/scope_in_javascript/
+// http://exploringjs.com/es6/ch_arrow-functions.html
+
+
+let events = require('events');
+let event_emitter = new events.EventEmitter();
+
+
+function BigComputer(answer) { 
+  this.the_answer = answer; 
+  this.ask_question = function() { 
+    // 42
+    console.log(this.the_answer);
+    // BigComputer { the_answer: 42, ask_question: [Function] }
+    console.log(this);
+  }
+}
+
+function add_handler() {
+  let deep_thought = new BigComputer(42);
+  // this = deep_thought (contrast w/ 06-complications.js)
+  event_emitter.on(
+    'event1',
+    () => deep_thought.ask_question()
+  );
+}
+
+add_handler();
+event_emitter.emit('event1', 54);
+
+
+// vim: set ts=2 sw=2 et:
+
+
